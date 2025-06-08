2295407d720002646ec1900e965cb58f0cb9b505
Sujan Dumaru <sdumaru30060@ucumberlands.edu>
2025-06-08 14:07:53 -0500

    Add comments

--- chatbot_project/chatbot_app/management/commands/chat.py
+++ chatbot_project/chatbot_app/management/commands/chat.py
@@ -19,35 +19,42 @@ class Command(BaseCommand):
             storage_adapter='chatterbot.storage.SQLStorageAdapter',
             logic_adapters=[
                 'chatterbot.logic.BestMatch',
-                'chatterbot.logic.TimeLogicAdapter',
-                'chatterbot.logic.MathematicalEvaluation'
+                'chatterbot.logic.TimeLogicAdapter',            # For time
+                'chatterbot.logic.MathematicalEvaluation'       # For mathematical operation
             ],
             database_uri='sqlite:///db.sqlite3'
         )
 
-        # Train on English corpus; comment out after first run to persist memory
+        # Train on English corpus
         trainer = ChatterBotCorpusTrainer(chatbot)
         trainer.train('chatterbot.corpus.english')
 
+        # Start chat session with success style message (green)
         self.stdout.write(self.style.SUCCESS(
             'Chat session started! (Type "exit" to quit or Ctrl-C to exit)\n'
         ))
 
-        # Start conversation with Chatterbot
+        # Start conversation with Chatterbot continuously
         while True:
             try:
                 user_input = input('You: ').strip()
+                # Make sure user input something to get response from bot
                 if not user_input:
                     continue
+
+                # Exit the chat session with exit command
                 if user_input.lower() == 'exit':
                     self.stdout.write('\nGoodbye!')
                     break
 
                 response = chatbot.get_response(user_input)
                 self.stdout.write(f'Bot: {response}')
+
+            # Exit with Ctrl + D command    
             except (KeyboardInterrupt, EOFError):
                 self.stdout.write('\nGoodbye!')
                 break
 
-        self.stdout.write('Exiting chat.')
+        # End chat session with error style message (red)
+        self.stdout.write(self.style.ERROR('Exiting chat!!!'))
         sys.exit(0)
